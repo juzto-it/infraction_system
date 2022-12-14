a99bd0f082c43fce4161b06aa1e2fdcce757645d
miguel <mbarrerop@gmail.com>
2022-12-14 14:08:34 -0500

    first commit consulta temp tuio

--- infraction_core/core_system/urls.py
+++ infraction_core/core_system/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('comparendos/', views.Fotomultas.as_view(), name='comparendos'),
+    path('multas-tuio/', views.MultasTuio.as_view(), name='multas-tuio'),
 ]
--- infraction_core/core_system/views.py
+++ infraction_core/core_system/views.py
@@ -10,6 +10,8 @@ from core_system.serializers.profiles import BasicProfileSerializer
 from core_system.serializers.personas import PersonasSerializer
 from core_system.models import *
 import pandas as pd
+import json
+import requests
 
 # Create your views here.
 
@@ -105,9 +107,98 @@ class MultasTuio(APIView):
     permission_classes = [IsAuthenticated]
     
     def post(self, request):
+        get_data = False
         fs_object = FileSystemStorage()
-        df_placas = pd.read_excel()
-        file_path_p = fs_object.base_location + 'Base de Datos JUZTO.xlsx'
+        cols = ['fecha_consulta',
+                'comparendo',
+                'placa',
+                'nit',
+                'estado_comparendo',
+                'comparendo_electronico',
+                'fecha_comparendo',
+                'tipo_infraccion',
+                'ciudad_infraccion',
+                'tiene_resolucion',
+                'fecha_resolucion',
+                'tiene_cobro_coactivo',
+                'fecha_cobro_coactivo',
+                'valor_infraccion',
+                'fecha_notificacion',
+                'valor_notificacion',
+                'dias_notificacion',
+                'fecha_descuento_50',
+                'valor_descuento_50',
+                'dias_descuento_50',
+                'fecha_descuento_25',
+                'valor_descuento_25',
+                'dias_descuento_25',
+                'fecha_sin_intereses',
+                'valor_sin_intereses',
+                'dias_sin_intereses',
+                
+
+                ]
+        df_multas = pd.DataFrame(columns=cols)
+        file_path_p = fs_object.base_location + '/Base de Datos JUZTO.xlsx'
+        df_placas = pd.read_excel(file_path_p, sheet_name='Hoja1', 
+                                  usecols=['Placa'])
+        hds = {'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJjbGllbnRJZCI6IjYxNjlmMmE3ZmU0ZjY2M2MyZmZkMDZmNCIsImRvY3VtZW50VHlwZSI6Ik5JVCIsImRvY3VtZW50TnVtYmVyIjoiOTAxMzUwNjI4LTQiLCJ2IjoxLCJyb2xlIjoiY2xpZW50Iiwic3Vic2NyaXB0aW9uUGxhbiI6IjYxNjlmMmE4NDhmOGI5N2QzZDliZGIxZCIsIkpXVFBocmFzZSI6ImFwcjI1IiwiaWF0IjoxNjU4MTcxMTA5fQ.hLzhjs_dtwR8o2J6CVHyvat9c1gqZfkgK7NqRkRmJXU' , 
+               'Content-Type': 'application/json'}
         
-        Response(status=status.HTTP_200_OK, data={'object': 'finalizado'}) 
+        IUtility.format_date('04/11/2022')
+        print(df_placas)
+        for index, rec in df_placas.iterrows():
+
+            num_placa = rec['Placa']
+  
+            
+            body = dict(documentType='CC', documentNumber=num_placa)
+            url = 'https://api.verifik.co/v2/co/simit/consultar'
+            try:
+                response = json.loads(requests.get(url=url, headers=hds, data=json.dumps(body).encode('utf8')).text)
+                LogMultas.objects.create(placa=num_placa, fecha_consulta=IUtility.datetime_utc_now(), resultado='exitoso')
+            except Exception as err:
+                LogMultas.objects.create(placa=num_placa, fecha_consulta=IUtility.datetime_utc_now(), resultado='fallido')
+                continue
+            if response.get('data').get('multas'):
+                multas = response.get('data').get('multas')
+                for multa in multas:
+                    try:
+                        data_multa = {
+                        'fecha_consulta': IUtility.datetime_utc_now(),
+                            'id_comparendo': multa.get('numeroComparendo'),
+                            'placa': num_placa,
+                            'documento': multa.get('infractor').get('numeroDocumento'),
+                            'estado_comparendo': multa.get('estadoComparendo'),
+                            'comparendo_electronico': multa.get('comparendoElectronico'),
+                            'fecha_comparendo': IUtility.format_date(multa.get('fechaComparendo')),
+                            'tipo_infraccion':multa.get('infracciones')[0].get('codigoInfraccion'),
+                            'ciudad_infraccion': multa.get('organismoTransito'),
+                            'tiene_resolucion': multa.get('numeroResolucion'),
+                            'fecha_resolucion': IUtility.format_date(multa.get('fechaResolucion')),
+                            'tiene_cobro_coactivo': multa.get('nroCoactivo'),
+                            'fecha_cobro_coactivo': IUtility.format_date(multa.get('fechaCoactivo')),
+                            'valor_infraccion': multa.get('valorPagar'),
+                            'fecha_notificacion': IUtility.format_date(multa.get('proyeccion')[0].get('fecha')) if len(multa.get('proyeccion')) > 0 else None,
+                            'valor_notificacion': multa.get('proyeccion')[0].get('valor') if len(multa.get('proyeccion')) > 0 else None,
+                            'dias_notificacion': multa.get('proyeccion')[0].get('dias') if len(multa.get('proyeccion')) > 0 else None,
+                            'fecha_descuento_50': IUtility.format_date(multa.get('proyeccion')[1].get('fecha')) if len(multa.get('proyeccion')) > 1 else None,
+                            'valor_descuento_50': multa.get('proyeccion')[1].get('valor') if len(multa.get('proyeccion')) > 1 else None,
+                            'dias_descuento_50':  multa.get('proyeccion')[1].get('dias') if len(multa.get('proyeccion')) > 1 else None,
+                            'fecha_descuento_25': IUtility.format_date(multa.get('proyeccion')[2].get('fecha')) if len(multa.get('proyeccion')) > 2 else None,
+                            'valor_descuento_25': multa.get('proyeccion')[2].get('valor') if len(multa.get('proyeccion')) > 2 else None,
+                            'dias_descuento_25': multa.get('proyeccion')[2].get('dias') if len(multa.get('proyeccion')) > 2 else None,
+                            'fecha_sin_intereses': IUtility.format_date(multa.get('proyeccion')[3].get('fecha')) if len(multa.get('proyeccion')) > 3 else None,
+                            'valor_sin_intereses': multa.get('proyeccion')[3].get('valor')if len(multa.get('proyeccion')) > 3 else None,
+                            'dias_sin_intereses':multa.get('proyeccion')[3].get('dias') if len(multa.get('proyeccion')) > 3 else None,
+                            
+                        }
+                        Multas.objects.update_or_create(id_comparendo=data_multa.get('id_comparendo'), defaults=data_multa)
+                    except Exception as e:
+                        print(e)
+                    #df_multas = df_multas.append(data_multa, ignore_index=True)
+
+        df_multas.to_csv('resultados_consulta.csv')
+        print(df_multas)
+        return Response(status=status.HTTP_200_OK, data={'object': 'finalizado'}) 
         
